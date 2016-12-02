af01fb48e8e18e1e754c0e39ff003b6c708fcda7
Sam Sandberg <sam.sandberg@gmail.com>
2016-12-02 12:29:57 -0500

    2016, day 1, part 2

--- 2016/day01/solution.rb
+++ 2016/day01/solution.rb
@@ -1,4 +1,5 @@
 
+require 'set'
 
 UP = 0
 RIGHT = 1
@@ -32,40 +33,61 @@ def input_to_directions(input)
 end
 
 def get_directions_test_1()
-    return input_to_directions("R2, L3")
+    return "R2, L3"
 end
 
 def get_directions_test_2()
-    return input_to_directions("R2, R2, R2")
+    return "R2, R2, R2"
 end
 
 def get_directions_test_3()
-    return input_to_directions("R5, L5, R5, R3")
+    return "R5, L5, R5, R3"
+end
+
+def get_directions_test_4()
+    return "R8, R4, R4, R8"
 end
 
 def get_directions()
-    return input_to_directions(File.read('input.txt'))
+    return File.read('input.txt')
+end
+
+def coordinate_to_s(x, y)
+    return "#{x},#{y}"
 end
 
 def main()
     direction_current = UP
     x = y = 0
-    directions = get_directions
+    coordinate = coordinate_to_s(x, y)
+    coordinates_seen = Set.new([coordinate])
+    has_seen_dupe = false
+    input = get_directions
+    # puts input
+    directions = input_to_directions(input)
     directions.each do |direction|
         turn = direction[0]
         blocks = direction[1..-1].to_i
         # puts direction_current, turn, blocks
         direction_current = MAP_TURN[turn][direction_current]
-        if direction_current == UP
-            y += blocks
-        elsif direction_current == RIGHT
-            x += blocks
-        elsif direction_current == DOWN
-            y -= blocks
-        elsif direction_current == LEFT
-            x -= blocks
-        else
-            raise "INVALID DIRECTION!!!"
+        blocks.times do
+            if direction_current == UP
+                y += 1
+            elsif direction_current == RIGHT
+                x += 1
+            elsif direction_current == DOWN
+                y -= 1
+            elsif direction_current == LEFT
+                x -= 1
+            else
+                raise "INVALID DIRECTION!!!"
+            end
+            coordinate = coordinate_to_s(x, y)
+            if coordinates_seen.add?(coordinate).nil? and not has_seen_dupe
+                distance = x.abs + y.abs
+                puts "First dupe coordinate: (#{x}, #{y}) (distance: #{distance})"
+                has_seen_dupe = true
+            end
         end
     end
     puts "End coordinates: (#{x}, #{y})"

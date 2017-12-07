07a8c0b0446528093e53c248a1243d20c172ef61
Sam Sandberg <sam.sandberg@gmail.com>
2017-12-07 11:03:24 -0500

    Annotate part 1 w/ better comments

--- 2017/day07/solution.cpp
+++ 2017/day07/solution.cpp
@@ -21,16 +21,18 @@ void handle_line(string line) {
     for (int counter = 0; stream >> word; counter++) {
         switch (counter) {
             case 0:
+                // Parent: fwft
                 parent = word;
                 // cout << "Parent " << parent << endl;
                 break;
             case 1:
-                // Weight
+                // Weight: (72)
             case 2:
-                // Arrow
+                // Arrow: ->
                 break;
             default:
                 // Child, potentially with trailing comma
+                //   ktlj, cntj, xhth
                 if (word[word.length() - 1] == ',') {
                     word = word.substr(0, word.length() - 1);
                 }
@@ -42,13 +44,16 @@ void handle_line(string line) {
 
 
 string part1() {
+    // Grab any item's parent to start
     string item = parents.begin()->second;
     while (true) {
+        // Search for that parent's parent
         auto search = parents.find(item);
-        // Found it!
+        // Once an item doesn't have a parent, we're at the root
         if (search == parents.end()) {
             return item;
         }
+        // Otherwise keep going up the tree
         item = search->second;
     }
 }

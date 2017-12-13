b344c3e00c3f0eda3fcacc455bc114382a465aab
Sam Sandberg <sam.sandberg@gmail.com>
2017-12-13 16:47:08 -0500

    Day 12, parts 1 and 2, README

--- 2017/day12/solution.cpp
+++ 2017/day12/solution.cpp
@@ -1,15 +1,19 @@
+/**
+ * $ g++ solution.cpp && ./a.out < input.txt 
+ * Day 12!
+ * Part 1: 288
+ * Part 2: 211
+ */
 
-
+#include <deque>
 #include <iostream>
 #include <string>
 #include <sstream>
 #include <unordered_set>
-#include <vector>
 
 using namespace std;
 
-
-vector<unordered_set<int>> sets;
+deque<unordered_set<int>> sets;
 
 
 /**
@@ -47,41 +51,65 @@ void handle_line(string line) {
 }
 
 
+/**
+ * Look for another set with overlapping elements and merge this set in.
+ * Returns whether a merge occured;
+ */
+bool merge_set(unordered_set<int> set) {
+    for (int i = 0; i < sets.size(); i++) {
+        for (auto item_search = set.begin(); item_search != set.end(); item_search++) {
+            auto search = sets[i].find(*item_search);
+            // Found it, merge this set in
+            if (search != sets[i].end()) {
+                // https://stackoverflow.com/questions/7089494/merge-multiple-sets-elements-in-a-single-set/7089564#7089564
+                sets[i].insert(set.begin(), set.end());
+                return true;
+            }
+        }
+    }
+    // Didn't merge. Push onto back of sets.
+    sets.push_back(set);
+    return false;
+}
+
+
+/**
+ * http://www.cplusplus.com/reference/deque/deque/
+ */
+void build_groups() {
+    unordered_set<int> set;
+    int num_merge_fails = 0;
+    while (num_merge_fails < sets.size()) {
+        set = sets[0];
+        sets.pop_front();
+        // Only one set left!
+        if (sets.size() == 0) {
+            sets.push_back(set);
+            return;
+        }
+        if (merge_set(set)) {
+            num_merge_fails = 0;
+        } else {
+            num_merge_fails++;
+        }
+    }
+}
+
+
 int part1() {
-    unordered_set<int> connected;
-    int set_size;
-    do {
-        set_size = connected.size();
-        // cout << "set_size: " << set_size << endl;
-        // First find the set containing `0`
-        if (set_size == 0) {
-            for (int i = 0; i < sets.size(); i++) {
-                auto search = sets[i].find(0);
-                // Found it!
-                if (search != sets[i].end()) {
-                    connected = sets[i];
-                    break;
-                }
-            }
-            if (connected.size() == 0) {
-                // OH NOH!
-                throw 100;
-            }
+    for (int i = 0; i < sets.size(); i++) {
+        auto search = sets[i].find(0);
+        // Found it, merge this set in
+        if (search != sets[i].end()) {
+            return sets[i].size();
         }
-        // Otherwise look through all sets for any intersection
-        for (int i = 0; i < sets.size(); i++) {
-            for (auto item_search = connected.begin(); item_search != connected.end(); item_search++) {
-                auto search = sets[i].find(*item_search);
-                // Found it, merge this set in
-                if (search != sets[i].end()) {
-                    // https://stackoverflow.com/questions/7089494/merge-multiple-sets-elements-in-a-single-set/7089564#7089564
-                    connected.insert(sets[i].begin(), sets[i].end());
-                    break;
-                }
-            }
-        }
-    } while (set_size != connected.size());
-    return set_size;
+    }
+    return 0;
+}
+
+
+int part2() {
+    return sets.size();
 }
 
 
@@ -91,5 +119,7 @@ int main() {
     while (getline(cin, line)) {
         handle_line(line);
     }
+    build_groups();
     cout << "Part 1: " << part1() << endl;
+    cout << "Part 2: " << part2() << endl;
 }

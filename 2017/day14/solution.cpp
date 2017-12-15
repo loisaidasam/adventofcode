e7a4c0e20db16a0d9bf2443c57e97218e5c933fe
Sam Sandberg <sam.sandberg@gmail.com>
2017-12-15 00:46:57 -0500

    Re-usable day 14 part 1

--- 2017/day14/solution.cpp
+++ 2017/day14/solution.cpp
@@ -10,6 +10,9 @@
 using namespace std;
 
 
+int binary_values[128][128];
+
+
 string hex_to_binary(char c) {
     switch(toupper(c)) {
         case '0': return "0000";
@@ -33,20 +36,41 @@ string hex_to_binary(char c) {
 }
 
 
-int part1(string input) {
-    int used = 0;
-    for (int i = 0; i < 128; i++) {
-        string hash_input = input + "-" + to_string(i);
+void set_values(string input) {
+    for (int row = 0; row < 128; row++) {
+        string hash_input = input + "-" + to_string(row);
         // cout << "hash_input " << hash_input << endl;
         string hash = knot_hash(hash_input);
         // cout << "hash " << hash << endl;
-        for (int i = 0; i < hash.length(); i++) {
-            string binary = hex_to_binary(hash[i]);
-            // cout << "\t" << i << ".\t" << binary << endl;
-            for (int j = 0; j < binary.length(); j++) {
-                if (binary[j] == '1') {
-                    used++;
+        for (int hash_letter_num = 0; hash_letter_num < hash.length(); hash_letter_num++) {
+            string binary = hex_to_binary(hash[hash_letter_num]);
+            // cout << "\t" << hash_letter_num << ".\t" << binary << endl;
+            if (binary.length() != 4) {
+                cout << "Unknown binary length (not 4): " << binary << endl;
+                throw 150;
+            }
+            for (int binary_col_num = 0; binary_col_num < 4; binary_col_num++) {
+                int col = (hash_letter_num * 4) + binary_col_num;
+                if (binary[binary_col_num] == '0') {
+                    binary_values[row][col] = 0;
+                } else if (binary[binary_col_num] == '1') {
+                    binary_values[row][col] = 1;
+                } else {
+                    cout << "Unknown binary value: " << binary[binary_col_num] << endl;
+                    throw 400;
                 }
+            }
+        }
+    }
+}
+
+
+int part1() {
+    int used = 0;
+    for (int i = 0; i < 128; i++) {
+        for (int j = 0; j < 128; j++) {
+            if (binary_values[i][j] == 1) {
+                used++;
             }
         }
     }
@@ -54,9 +78,15 @@ int part1(string input) {
 }
 
 
+int part2(string input) {
+    return 0;
+}
+
+
 int main() {
+    cout << "Day 14!" << endl;
     string input;
     cin >> input;
-    cout << "Day 14!" << endl;
-    cout << "Part 1: " << part1(input) << endl;
+    set_values(input);
+    cout << "Part 1: " << part1() << endl;
 }

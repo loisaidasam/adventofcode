c8cbf88509bb66a6f57ad18b0b84d4b52283d6a9
Sam Sandberg <sam.sandberg@gmail.com>
2017-12-17 00:56:42 -0500

    Day 15, both parts

--- 2017/day15/solution.cpp
+++ 2017/day15/solution.cpp
@@ -2,6 +2,7 @@
  * $ g++ solution.cpp && ./a.out
  * Day 15!
  * Part 1: 631
+ * Part 2: 279
  */
 
 #include <iostream>
@@ -21,20 +22,20 @@ const uint64_t DIVISOR = 2147483647;
 // Generator B starts with 583
 const uint64_t START_A = 873;
 const uint64_t START_B = 583;
-const int NUM_ROUNDS = 40000000;
 
 // TESTING:
 // const uint64_t START_A = 65;
 // const uint64_t START_B = 8921;
-// const int NUM_ROUNDS = 5;
 
 
 int part1() {
+    const int NUM_ROUNDS = 40000000;
+    // const int NUM_ROUNDS = 5;
     int count = 0;
     uint64_t a = START_A;
     uint64_t b = START_B;
-    string str_a;
-    string str_b;
+    // string str_a;
+    // string str_b;
     for (int round = 1; round <= NUM_ROUNDS; round++) {
         // if (round % 100000 == 0) {
         //     cout << "round " << round << endl;
@@ -57,6 +58,34 @@ int part1() {
 }
 
 
+int part2() {
+    const int NUM_ROUNDS = 5000000;
+    int count = 0;
+    uint64_t a = START_A;
+    uint64_t b = START_B;
+    for (int round = 1; round <= NUM_ROUNDS; round++) {
+        // if (round % 100000 == 0) {
+        //     cout << "round " << round << endl;
+        // }
+
+        // Divisible by 4
+        do {
+            a = (a * FACTOR_A) % DIVISOR;
+        } while (bitset<2>(a) != 0);
+
+        // Divisible by 8
+        do {
+            b = (b * FACTOR_B) % DIVISOR;
+        } while (bitset<3>(b) != 0);
+
+        if (bitset<16>(a) == bitset<16>(b)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+
 int main() {
     cout << "Day 15!" << endl;
     // cout << 16807 << " " << bitset<32>(16807) << " " << bitset<4>(16807) << endl;
@@ -65,4 +94,6 @@ int main() {
     // bool yas = (bitset<3>(17) == bitset<3>(33));
     // cout << yas << endl;
     cout << "Part 1: " << part1() << endl;
+    // cout << (bitset<3>(4) == 0) << endl;
+    cout << "Part 2: " << part2() << endl;
 }

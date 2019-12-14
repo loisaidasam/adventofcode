83b0edaacfcd2f4355b36d6f9c00d073fdb8242c
LoisaidaSam <sam.sandberg@gmail.com>
2019-12-14 15:39:00 -0500

    `SolutionX.java` template

---
+++ 2019/src/common/SolutionX.java
@@ -0,0 +1,23 @@
+package common;
+
+public class SolutionX extends BaseSolution implements Solution {
+
+    public static void main(String[] args) throws Exception {
+        new SolutionX().run();
+    }
+
+//    public SolutionX() {
+//        super();
+//        inputFilename = "input-test-1.txt";
+//    }
+
+    @Override
+    public String part1() throws Exception {
+        return null;
+    }
+
+    @Override
+    public String part2() throws Exception {
+        return null;
+    }
+}

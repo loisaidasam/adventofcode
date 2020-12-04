2e0306ec1b84bea776b9e0983fbda64f0950424a
Banana Sam Sandberg <ssandberg@pandora.com>
2020-12-04 09:55:42 -0500

    2019 READMEs/inputs

---
+++ 2019/src/day17/Solution17.java
@@ -0,0 +1,36 @@
+package day17;
+
+import common.BaseSolution;
+import common.Solution;
+
+public class Solution17 extends BaseSolution implements Solution {
+
+    public static void main(String[] args) throws Exception {
+        new Solution17().run();
+    }
+
+//    public SolutionX() {
+//        super();
+//        inputFilename = "input-test-1.txt";
+//    }
+
+    @Override
+    public String part1() throws Exception {
+        // ASCII code to String
+        // via https://stackoverflow.com/a/7693999/1406873
+        // Character.toString ((char) i);
+
+        // System.out.println((char)65); would print "A"
+
+        // int number = 65;
+        // char c = (char)number;
+
+        // new String(new char[] { 65 })
+        return null;
+    }
+
+    @Override
+    public String part2() throws Exception {
+        return null;
+    }
+}

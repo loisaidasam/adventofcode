c7f67c5b54e42446f644b1e796763743401eadbc
Banana Sam Sandberg <ssandberg@pandora.com>
2020-12-04 15:40:03 -0500

    Day 3

---
+++ 2020/src/day03/Solution03.java
@@ -0,0 +1,56 @@
+package day03;
+
+import common.BaseSolution;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution03 extends BaseSolution {
+
+    public static void main(String[] args) throws Exception {
+        new Solution03().run();
+    }
+
+    protected InputType getInputType() {
+        return InputType.STRINGS;
+    }
+
+    @Override
+    protected Integer part1Int() throws Exception {
+        return tobogganRun(3, 1);
+    }
+
+    @Override
+    protected Integer part2Int() {
+        List<Integer> results = new ArrayList<>();
+        results.add(tobogganRun(1, 1));
+        results.add(tobogganRun(3, 1));
+        results.add(tobogganRun(5, 1));
+        results.add(tobogganRun(7, 1));
+        results.add(tobogganRun(1, 2));
+        return results.stream()
+                .reduce(1, (a, b) -> a * b);
+    }
+
+    private int tobogganRun(int right, int down) {
+        int numTrees = 0;
+        int row = 0;
+        int col = 0;
+        final int WIDTH = inputStrs.get(0).length();
+        while (row < inputStrs.size()) {
+            if (isTree(row, col)) {
+                numTrees++;
+            }
+            col += right;
+            row += down;
+            if (col >= WIDTH) {
+                col -= WIDTH;
+            }
+        }
+        return numTrees;
+    }
+
+    private boolean isTree(int row, int col) {
+        return inputStrs.get(row).charAt(col) == '#';
+    }
+}

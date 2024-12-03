5d03070cf30434f44e405608655af0a41d182209
Sam Sandberg <ssandberg@pandora.com>
2024-12-02 21:43:42 -0500

    Solution template

---
+++ 2024/solution-template.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int solution1() {
+    return 0;
+}
+
+int solution2() {
+    return 0;
+}
+
+int main() {
+    cout << solution1() << endl;
+    // cout << solution2() << endl;
+    return 0;
+}

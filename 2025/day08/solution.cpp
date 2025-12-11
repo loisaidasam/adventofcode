76bd594a86427dfdfe7a5a314d9ae8a74ec15eab
LoisaidaSam <sam.sandberg@gmail.com>
2025-12-10 23:28:56 -0500

    Day 8 part 2

--- 2025/day08/solution.cpp
+++ 2025/day08/solution.cpp
@@ -35,6 +35,21 @@ void print_pos(Position p) {
 }
 
 
+vector<Position> read_positions() {
+    vector<Position> positions;
+    string line;
+    while (getline(cin, line)) {
+        stringstream ss(line);
+        string x, y, z;
+        getline(ss, x, ',');
+        getline(ss, y, ',');
+        getline(ss, z, ',');
+        positions.push_back({stoi(x), stoi(y), stoi(z)});
+    }
+    return positions;
+}
+
+
 // We'll skip square rooting because relative distances will be the same
 long distance_squared(const Position p1, const Position p2) {
     long dx = p2.x - p1.x;
@@ -47,6 +62,7 @@ long distance_squared(const Position p1, const Position p2) {
 struct Node {
     int i;
     set<int> edges;
+    Position position;
 };
 
 
@@ -77,16 +93,7 @@ set<int> traverse_circuit(Node boxes[], int i) {
 
 
 int solution1() {
-    vector<Position> positions;
-    string line;
-    while (getline(cin, line)) {
-        stringstream ss(line);
-        string x, y, z;
-        getline(ss, x, ',');
-        getline(ss, y, ',');
-        getline(ss, z, ',');
-        positions.push_back({stoi(x), stoi(y), stoi(z)});
-    }
+    vector<Position> positions = read_positions();
     const int NUM_BOXES = positions.size();
     Node boxes[NUM_BOXES];
     vector<pair<long, pair<int, int>>> sims;
@@ -137,12 +144,47 @@ int solution1() {
     return result;
 }
 
-int solution2() {
-    return 0;
+
+long solution2() {
+    vector<Position> positions = read_positions();
+    const int NUM_BOXES = positions.size();
+    Node boxes[NUM_BOXES];
+    vector<pair<long, pair<int, int>>> sims;
+    for (int i = 0; i < NUM_BOXES; i++) {
+        // cout << i << ": ";
+        // print_pos(positions[i]);
+        // cout << endl;
+        boxes[i].i = i;
+        boxes[i].position = positions[i];
+        for (int j = i+1; j < NUM_BOXES; j++) {
+            long distance = distance_squared(positions[i], positions[j]);
+            sims.push_back(make_pair(distance, make_pair(i, j)));
+        }
+    }
+    sort(sims.begin(), sims.end());
+    for (int i = 0; i < sims.size(); i++) {
+        // cout << "sim " << i << ": " << sims[i].second.first << "=";
+        // print_pos(positions[sims[i].second.first]);
+        // cout << ", " << sims[i].second.second << "=";
+        // print_pos(positions[sims[i].second.second]);
+        // cout << " sim=" << sims[i].first << endl;
+        int p1 = sims[i].second.first;
+        int p2 = sims[i].second.second;
+        boxes[p1].edges.insert(boxes[p2].i);
+        boxes[p2].edges.insert(boxes[p1].i);
+        set<int> circuit = traverse_circuit(boxes, 0);
+        if (circuit.size() == NUM_BOXES) {
+            long x1 = boxes[p1].position.x;
+            long x2 = boxes[p2].position.x;
+            return x1 * x2;
+        }
+    }
+    // Getting here would be weird - we should never actually exhaust ALL sims...
+    return -1;
 }
 
 int main() {
-    cout << solution1() << endl;
-    // cout << solution2() << endl;
+    // cout << solution1() << endl;
+    cout << solution2() << endl;
     return 0;
 }
